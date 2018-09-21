df6e64f1dbc03b95b8b7644d4bf256e13e47f931
Austin Melms <memyselfandlie@gmail.com>
2018-09-21 05:57:08 -0400

    removed duplicate code for misc util target

--- src/commands/funCommands/howgay.js
+++ src/commands/funCommands/howgay.js
@@ -2,13 +2,7 @@ const GenericCommand = require('../../models/GenericCommand')
 
 module.exports = new GenericCommand(
   async ({ Memer, msg, args }) => {
-    let target = !args[0] || args[0].toLowerCase() === 'me'
-      ? 'You are'
-      : (
-        msg.mentions[0]
-          ? `${msg.mentions[0].nick || msg.mentions[0].username} is`
-          : `${args.join(' ')} is`
-      )
+    let target = Memer.getRateTarget(msg, args)
     const rating = Memer.randomNumber(1, 100)
     return {
       title: 'gay r8 machine',
